de5029fc7adcf43074d0df94509b7288f36e3c98
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-23 21:10:55 +0800

    feat: Seeding, migrations

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    use HasFactory;
+
     //
     protected $fillable = [
         'name',
         'title',
+        'published_date'
     ];
 
     // A book belongs to an author.
---
+++ database/factories/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use App\Models\Author;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'title' => fake()->name(),
+            'author_id' => Author::all()->random()->id,
+            'published_date' => fake()->date(),
+        ];
+    }
+}
