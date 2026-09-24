a209c9e2c6e03e3879c73bbc5c555057ac402c8b
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-24 14:35:25 +0800

    feat: Routes for webpages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+use App\Models\Author;
+use App\Models\Book;
 
 Route::get('/', function () {
+    $authors = Author::all();
+    // return view('authors.index', ['authors' => $authors]);
     return view('welcome');
+});
+
+Route::get('authors', function () {
+    $authors = Author::all();
+    return view('authors.index', ['authors' => $authors]);
+});
+
+Route::get('authors/create', function () {
+    return view('authors.create');
+});
+
+Route::get('books', function () {
+    $books = Book::all();
+    return view('books.index', ['books' => $books]);
+});
+
+Route::get('/token', function () {
+    return csrf_token(); 
 });
