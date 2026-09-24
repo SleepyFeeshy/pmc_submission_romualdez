3ba93b6176ec77dc0f7ec74adf97a19a0e37dd03
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-24 19:38:49 +0800

    fix: Authors Update form values

--- resources/views/authors/edit.blade.php
+++ resources/views/authors/edit.blade.php
@@ -1,23 +1,22 @@
 <htmL>
     <body>
         <h1>Editing</h1>
-        <form method="PUT" action="{{ route('authors.update', $author->id) }}">
+        <form method="POST" action="{{ route('authors.update', $author->id) }}">
             @method('PUT')
             @csrf
             <input
                 id="name"
                 type="text"
                 name="name"
-                value={{ $author->name }}
+                value="{{ $author->name }}"
             >
             <input
                 id="birth_date"
                 type="date"
                 name="birth_date"
-                value={{ $author->birth_date }}
+                value="{{ $author->birth_date }}"
             >
             <button type="submit"> Submit </button>
-            </input>
         </form>
     </body>
 </html>
