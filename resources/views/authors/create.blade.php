0fbb3060dd208d362696b58e3ad1ea53712c0bd7
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-24 19:38:24 +0800

    feat: data validation error message draft

--- resources/views/authors/create.blade.php
+++ resources/views/authors/create.blade.php
@@ -1,4 +1,16 @@
-<x-layout>
+@extends('layouts.app')
+
+@section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    
     <form method="POST" action="{{ route('authors.store') }}">
         @method('POST')
         @csrf
@@ -13,6 +25,7 @@
             name="birth_date"
         >
         <button type="submit"> Submit </button>
-        </input>
     </form>
-</x-layout>
+
+
+@endsection
