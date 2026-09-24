98bd5f1e00e76bf4c67c20d60794c83a92f4ad6c
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-25 07:16:47 +0800

    refactor: AJAX to fetch data

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -1,24 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-    <table>
+    <script>
+        const loadAuthors = async function() {
+            const tableBody = document.getElementById('table-body');
+            const response = await fetch("/api/authors")
+            const authors = await response.json();
+
+            authors.forEach(function(author) {
+                const row = 
+                `
+                    <tr>
+                        <td>${author.id} </td>
+                        <td>${author.name} </td>
+                        <td>${author.birth_date} </td>
+                        <td>${author.created_at} </td>
+                        <td>${author.updated_at} </td>
+                        <td> 
+                            <a class="text-sm" href=""> View </a> 
+                            <a class="text-sm" href=""> Edit </a>
+                            <a class="text-sm" href=""> Delete </a>
+                        </td>
+                    </tr>
+                `
+                tableBody.insertAdjacentHTML('beforeend', row);
+            })
+        };
+        $(document).ready(loadAuthors);
+    </script>
+    <table id="table-body">
         <tr>
             <th> id </th>
             <th> name </th>
             <th> birth_date </th>
             <th> created_at </th>
             <th> updated_at </th>
+            <th> Actions </th>
         </tr>
-        @foreach ($authors as $author)
+        <div>
+        </div >
+        {{-- @foreach ($authors as $author)
         <tr>
             <td>{{ $author->id }} </td>
             <td>{{ $author->name }} </td>
             <td>{{ $author->birth_date }} </td>
             <td>{{ $author->created_at }} </td>
             <td>{{ $author->updated_at }} </td>
-            <td> <a href="{{ route('authors.edit', $author->id) }}"> Edit </a> </td>
+            <td> 
+                <a class="text-sm" href="{{ route('authors.show', $author->id) }}"> View </a> 
+                <a class="text-sm" href="{{ route('authors.edit', $author->id) }}"> Edit </a>
+                <a class="text-sm" href="{{ route('authors.destroy', $author->id) }}"> Delete </a>
+            </td>
         </tr>
-        @endforeach
+        @endforeach --}}
     </table>
     <a href="/authors/create">Add author</a>
 @endsection
