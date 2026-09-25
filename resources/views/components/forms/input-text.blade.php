9ca47b3b98eb0681bee1f4fe42fa308fa0cb7eee
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-25 11:30:52 +0800

    feat: value prop for InputText component

--- resources/views/components/forms/input-text.blade.php
+++ resources/views/components/forms/input-text.blade.php
@@ -1,9 +1,13 @@
-@props(['id', 'placeholder', 'name'])
+@props(['id', 'placeholder', 'name', 'value'])
 
+<label class="text-sm text-gray-700" for="name">
+    {{ ucwords($name) }}
+</label>
 <input
     class="bg-gray-100 p-1 w-full"
     id="{{ $id }}"
     type="text"
     placeholder="{{ $placeholder }}"
     name="{{ $name }}"
+    value="{{ $value }}"
 >
