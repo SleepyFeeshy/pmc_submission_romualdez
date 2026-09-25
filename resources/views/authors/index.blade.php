5081beaf3ec2f02017e6c74c56606a7049833749
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-25 09:44:34 +0800

    feat: AJAX Authors fetch and delete

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -2,31 +2,50 @@
 
 @section('content')
     <script>
+        const deleteAuthor = async function (id) {
+            response = await fetch('/api/authors/' + id, {
+                method: 'DELETE',
+            })
+        };
+
         const loadAuthors = async function() {
             const tableBody = document.getElementById('table-body');
             const response = await fetch("/api/authors")
             const authors = await response.json();
 
+            tableBody.innerHTML = "";
             authors.forEach(function(author) {
                 const row = 
                 `
-                    <tr>
-                        <td>${author.id} </td>
+                    <tr class="odd:bg-white even:bg-slate-50">
+                        <td class="py-2 px-1 font-mono">${author.id} </td>
                         <td>${author.name} </td>
-                        <td>${author.birth_date} </td>
-                        <td>${author.created_at} </td>
-                        <td>${author.updated_at} </td>
+                        <td class="font-mono text-gray-500">${author.birth_date} </td>
+                        <td class="font-mono text-gray-500">${author.created_at} </td>
+                        <td class="font-mono text-gray-500">${author.updated_at} </td>
                         <td> 
-                            <a class="text-sm" href=""> View </a> 
-                            <a class="text-sm" href=""> Edit </a>
-                            <a class="text-sm" href=""> Delete </a>
+                            <a class="text-sm" href="api/authors/${author.id}/show"> View </a> 
+                            <a class="text-sm" href="api/authors/${author.id}/edit"> Edit </a>
+                            <button class="delete-btn font-sm p-1 rounded-sm bg-red-700 hover:bg-red-800 text-white font-semibold hover:cursor-pointer" data-id="${author.id}"> Delete </button>
                         </td>
                     </tr>
                 `
                 tableBody.insertAdjacentHTML('beforeend', row);
             })
         };
-        $(document).ready(loadAuthors);
+
+        $(document).ready(function() {
+            loadAuthors();
+
+            $('table').on('click', '.delete-btn', async function() {
+                // Read the attributes
+                let authorId = $(this).data('id');
+                console.log("Deleting record ID:", authorId);
+                await deleteAuthor(authorId);
+                await loadAuthors();
+            });
+        });
+        
     </script>
     <table id="table-body">
         <tr>
